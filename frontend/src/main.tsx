44062ad54746c36ee72feee230524ead508ca165
Oscar Miranda <oimirandaa08@gmail.com>
2025-11-23 14:17:28 -0600

    Clean up and created files

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,11 +4,6 @@ import './index.css'
 import App from './App.tsx'
 
 
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
-
-if (!PUBLISHABLE_KEY) {
-  throw new Error('Missing Publishable Key')
-}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
