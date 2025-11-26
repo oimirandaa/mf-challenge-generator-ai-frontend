29b665009e3fccf99572f0737d18d607cc2431ab
Oscar Miranda <oimirandaa08@gmail.com>
2025-11-26 17:19:54 -0600

    Build History Pannel

--- frontend/src/history/HistoryPanel.tsx
+++ frontend/src/history/HistoryPanel.tsx
@@ -1,3 +1,46 @@
+import {useEffect, useState} from "react";
+import {type Challenge, MCQChallenge} from "../challenge/MCQChallenge.tsx";
+
 export function HistoryPanel() {
-    return <></>
+    const [history, setHistory] = useState<Challenge[]>([])
+    const [isLoading, setIsLoading] = useState<boolean>(false)
+    const [error, setError] = useState<string | null>(null)
+    
+    useEffect(() => {
+        fetchHistory()
+    },[])
+    
+    const fetchHistory = async () => {
+        setIsLoading(true)
+    }
+    
+    if (isLoading) return <div>Loading History</div>
+    
+    if (error) {
+        return <div className="error-message">
+            <p>{error}</p>
+            <button onClick={() => fetchHistory()}>Retry</button>
+        </div>
+    }
+    
+    
+    return <>
+        <div className="history-panel">
+            <h2>History</h2>
+            {
+                history.length === 0 ? <p>No history available</p> :
+                    <div className="history-list">
+                        {
+                            history.map((challenge: Challenge) => {
+                                return <MCQChallenge
+                                        challenge={challenge}
+                                        key={challenge.id}
+                                        showExplanation
+                                                    />
+                            })
+                        }
+                    </div>
+            }
+        </div>
+    </>
 }
