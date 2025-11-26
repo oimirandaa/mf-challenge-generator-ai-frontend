312dd501cc5508a032bddbd296827a6dde2a3c8f
Oscar Miranda <oimirandaa08@gmail.com>
2025-11-26 17:20:50 -0600

    General UI builds

--- frontend/src/layout/LayOut.tsx
+++ frontend/src/layout/LayOut.tsx
@@ -1,3 +1,31 @@
+import {SignedIn, SignedOut, UserButton} from "@clerk/clerk-react";
+import {Link, Navigate, Outlet} from "react-router-dom";
+
 export function LayOut() {
-    return <></>
+    return <>
+        <div className="app-layout" >
+            {/*NavBar*/}
+            <header className="app-header">
+                <div className="header-content">
+                    <h1>Code Challenge Generator</h1>
+                    <nav>
+                        <SignedIn>
+                            <Link to="/">Generate Challenge</Link>
+                            <Link to="/history">History</Link>
+                            <UserButton/>
+                        </SignedIn>
+                    </nav>
+                </div>
+            </header>
+            
+            <main className="app-main">
+                <SignedOut>
+                    <Navigate to="/sign-in" replace/>
+                </SignedOut>
+                <SignedIn>
+                    <Outlet />
+                </SignedIn>
+            </main>
+        </div>
+    </>
 }
